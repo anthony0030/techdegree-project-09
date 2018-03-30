9e3a7060aa20f91a34aba77630afb63e33fc7dc7
Anthony Veaudry <anthony@veaudry.pro>
2018-03-30 10:59:08 +0300

    improve how the forms work
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,5 +1,5 @@
 const alertContainer = document.getElementById("alerts");
-
+const sendMessageButton = document.getElementById("sendMsg");
 
 // Close Button
 const closeButton = document.createElement("SPAN");
@@ -7,13 +7,19 @@ const closeButtonText = document.createTextNode(String.fromCharCode(215));
 closeButton.classList.add("closebtn");
 closeButton.appendChild(closeButtonText);
 
-// 
+// Event listener to remove the alerts when the closebutton is pressed
 alertContainer.addEventListener("click", function(event){
   if(event.target.className === "closebtn"){
     event.target.parentNode.parentNode.removeChild(event.target.parentNode)
   }
 });
 
+
+
+sendMessageButton.addEventListener("click", function(event){
+  event.preventDefault();
+  alertGen('Mesage Sent Sucsesfully!', 'success');
+});
 
 
 
@@ -23,8 +29,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // add the X button to the alerts
   var alerts = document.querySelectorAll("[class^='alert']");
   for (var i = 0; i < alerts.length; i++) {
-    var newCloseButton = closeButton.cloneNode(true)
-    alerts[i].appendChild(newCloseButton);
+    alerts[i].appendChild(closeButton.cloneNode(true));
   }
 
 // set the default view of the chart
@@ -35,15 +40,12 @@ drawLineChartWeekly()
 }); // end of document reddy
 
 
-
-
-
-
-function sendMessage(){
+// this prints out alerts
+function alertGen(say, style="default"){
   var alert = document.createElement("DIV");
-  var alertText = document.createTextNode("Mesage Sent Sucsesfully!");
+  var alertText = document.createTextNode(say);
   alert.appendChild(alertText);
-  alert.appendChild(closeButton);
-  alert.classList.add("alert--success");
+  alert.appendChild(closeButton.cloneNode(true));
+  alert.classList.add("alert--" + style);
   alertContainer.appendChild(alert)
 }
