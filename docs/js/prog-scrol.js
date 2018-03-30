2ceb2ee9385689467351bf777510668bcf6cf4f7
Anthony Veaudry <anthony@veaudry.pro>
2018-03-30 14:26:45 +0300

    WIP add scroll bar to indicat location on the page
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

---
+++ docs/js/prog-scrol.js
@@ -0,0 +1,1 @@
+window.onscroll=function(){var e=document.body.scrollTop||document.documentElement.scrollTop,t=document.documentElement.scrollHeight-document.documentElement.clientHeight,o=e/t*100;document.getElementById("myBar").style.width=o+"%"};
