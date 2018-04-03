7a432b43fa54b5a75d8d95ba501fa0e28661548b
Anthony Veaudry <anthony@veaudry.pro>
2018-04-03 12:27:04 +0300

    enable source maps for the processed js
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- docs/js/prog-scrol.js
+++ docs/js/prog-scrol.js
@@ -1,1 +1,2 @@
 window.onscroll=function(){var e=document.body.scrollTop||document.documentElement.scrollTop,t=document.documentElement.scrollHeight-document.documentElement.clientHeight,o=e/t*100;document.getElementById("myBar").style.width=o+"%"};
+//# sourceMappingURL=./prog-scrol.js.map
