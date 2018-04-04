09faa341bfc379d5802166e524fe391ba4f94a99
Anthony Veaudry <anthony@veaudry.pro>
2018-04-04 17:23:31 +0300

    move progscrol to the main script file
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- docs/js/script.js
+++ docs/js/script.js
@@ -106,6 +106,15 @@ modalContainer.addEventListener("click", function(event){
   }
 })
 
+// When the user scrolls the page, execute myFunction to set the progres on the scroll bars
+window.onscroll = function() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myHorizontalBar").style.width = scrolled + "%";
+  document.getElementById("myVerticalBar").style.height = scrolled + "%";
+};
+
 
 // if there are alerts make the green circle aprire on the bell
 function checkNotifications(){
