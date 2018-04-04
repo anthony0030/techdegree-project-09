aa2417ea9158be0b6b0d4aa18816a43654e36313
Anthony Veaudry <anthony@veaudry.pro>
2018-04-04 19:19:48 +0300

    remove duplicate code from the line drawer
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- source/js/chart-line.js
+++ source/js/chart-line.js
@@ -1,3 +1,36 @@
+Chart.defaults.global.elements.line.tension = 0;
+Chart.defaults.global.elements.line.borderColor = "rgb(115, 119, 189)";
+Chart.defaults.global.elements.line.backgroundColor = "rgba(115, 119, 189, 0.2)";
+Chart.defaults.global.elements.point.borderColor = "#7377bd";
+Chart.defaults.global.elements.point.style = "circle";
+Chart.defaults.global.elements.point.radius = 4;
+Chart.defaults.global.elements.point.borderWidth = 4;
+Chart.defaults.global.elements.point.backgroundColor = "#fff";
+Chart.defaults.global.legend.display = false;
+
+
+const options = {
+  scales: {
+    yAxes: [{
+      gridLines: {
+        drawTicks: false
+      }, //gridLines
+      ticks: {
+        beginAtZero: true,
+        callback: function(value, index, values) {
+          if (value !== 0) {return value;}
+        } //callback
+      }, //ticks
+    }], //yAxes
+    xAxes: [{
+      gridLines: {
+        drawTicks: false
+      } //gridLines
+    }] //xAxes
+  } //scales
+} //options
+
+
 function drawLineChartHourly() {
   localStorage.setItem("dailyTraficView", "hourly");
   new Chart(document.getElementById("chart-line"), {
@@ -6,45 +39,10 @@ function drawLineChartHourly() {
       labels: ["01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"],
       datasets: [{
         label: "Users",
-        data: [0, 75, 125, 100, 150, 200, 150, 175, 125, 175, 200, 175, 200, 75, 125, 100, 150, 200, 150, 175, 125, 175, 200, 175],
-        borderColor: "rgb(115, 119, 189)",
-        backgroundColor: "rgba(115, 119, 189, 0.2)",
-        lineTension: 0,
-        pointRadius: 4,
-        pointStyle: "circle",
-        pointBorderColor: "#7377bd",
-        pointBorderWidth: 4,
-        pointBackgroundColor: "#fff"
+        data: [0, 75, 125, 100, 150, 200, 150, 175, 125, 175, 200, 175, 200, 75, 125, 100, 150, 200, 150, 175, 125, 175, 200, 175]
       }]
     },
-    options: {
-      legend: {
-        display: false
-      },
-      scales: {
-        yAxes: [{
-          gridLines: {
-            drawTicks: false,
-            // offsetGridLines: true,
-          },
-          ticks: {
-            Min: 0,
-            suggestedMax: 250,
-            stepSize: 50,
-            beginAtZero: true,
-            callback: function(value, index, values) {
-              if (value !== 0) {return value;}
-            }
-          },
-        }],
-        xAxes: [{
-          gridLines: {
-            drawTicks: false,
-            // offsetGridLines: true,
-          }
-        }]
-      } //scales
-    } //options
+    options: options
   });
 }
 
@@ -56,148 +54,39 @@ function drawLineChartDaily() {
       labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
       datasets: [{
         label: "Users",
-        data: [0, 200, 500, 50, 300, 800, 1000],
-        borderColor: "rgb(115, 119, 189)",
-        backgroundColor: "rgba(115, 119, 189, 0.2)",
-        lineTension: 0,
-        pointRadius: 4,
-        pointStyle: "circle",
-        pointBorderColor: "#7377bd",
-        pointBorderWidth: 4,
-        pointBackgroundColor: "#fff"
+        data: [0, 200, 500, 50, 300, 800, 1000]
       }]
     },
-    options: {
-      legend: {
-        display: false
-      },
-      scales: {
-        yAxes: [{
-          gridLines: {
-            drawTicks: false,
-            // offsetGridLines: true,
-          },
-          ticks: {
-            Min: 0,
-            suggestedMax: 250,
-            stepSize: 100,
-            beginAtZero: true,
-            callback: function(value, index, values) {
-              if (value !== 0) {return value;}
-            }
-          },
-        }],
-        xAxes: [{
-          gridLines: {
-            drawTicks: false,
-            // offsetGridLines: true,
-          }
-        }]
-      } //scales
-    } //options
+    options: options
   });
 }
-
-
 
 function drawLineChartWeekly() {
   localStorage.setItem("dailyTraficView", "weekly");
   new Chart(document.getElementById("chart-line"), {
     type: "line",
     data: {
-      labels: [" ", "16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+      labels: ["01-07", "08-15", "16-24", "25-31"],
       datasets: [{
         label: "Users",
-        data: [0, 750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2000, 1750, 2000],
-        borderColor: "rgb(115, 119, 189)",
-        backgroundColor: "rgba(115, 119, 189, 0.2)",
-        lineTension: 0,
-        pointRadius: 4,
-        pointStyle: "circle",
-        pointBorderColor: "#7377bd",
-        pointBorderWidth: 4,
-        pointBackgroundColor: "#fff"
+        data: [0, 750, 1250, 1000]
       }]
     },
-    options: {
-      legend: {
-        display: false
-      },
-      scales: {
-        yAxes: [{
-          gridLines: {
-            drawTicks: false,
-            // offsetGridLines: true,
-          },
-          ticks: {
-            Min: 0,
-            suggestedMax: 2500,
-            stepSize: 500,
-            beginAtZero: true,
-            callback: function(value, index, values) {
-              if (value !== 0) {return value;}
-            }
-          },
-        }],
-        xAxes: [{
-          gridLines: {
-            drawTicks: false,
-            // offsetGridLines: true,
-          }
-        }]
-      } //scales
-    } //options
+    options: options
   });
 }
-
-
 
 function drawLineChartMonthly() {
   localStorage.setItem("dailyTraficView", "monthly");
   new Chart(document.getElementById("chart-line"), {
     type: "line",
     data: {
-      labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",],
+      labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
       datasets: [{
         label: "Users",
-        data: [0, 75, 125, 100, 150, 200, 150, 175, 125, 175, 200, 175],
-        borderColor: "rgb(115, 119, 189)",
-        backgroundColor: "rgba(115, 119, 189, 0.2)",
-        lineTension: 0,
-        pointRadius: 4,
-        pointStyle: "circle",
-        pointBorderColor: "#7377bd",
-        pointBorderWidth: 4,
-        pointBackgroundColor: "#fff"
+        data: [0, 75, 125, 100, 150, 200, 150, 175, 125, 175, 200, 175]
       }]
     },
-    options: {
-      legend: {
-        display: false
-      },
-      scales: {
-        yAxes: [{
-          gridLines: {
-            drawTicks: false,
-            // offsetGridLines: true,
-          },
-          ticks: {
-            Min: 0,
-            suggestedMax: 250,
-            stepSize: 50,
-            beginAtZero: true,
-            callback: function(value, index, values) {
-              if (value !== 0) {return value;}
-            }
-          },
-        }],
-        xAxes: [{
-          gridLines: {
-            drawTicks: false,
-            // offsetGridLines: true,
-          }
-        }]
-      } //scales
-    } //options
+    options: options
   });
 }
